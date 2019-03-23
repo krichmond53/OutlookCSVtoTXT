534701319ea99eb56395e6385f7349e31b86fbaf
Kevin Richmond <40185674+krichmond53@users.noreply.github.com>
2019-03-23 18:04:23 -0400

    Added Email class

---
+++ src/Email.java
@@ -0,0 +1,74 @@
+/*
+ * The email class will be constructed from the input .csv file for each line
+ * that is an actual email.
+ */
+
+public class Email {
+
+	private String dateTime;
+	private String subject;
+	private String body;
+	private String fromAddress;
+	private String toAddress;
+	private String ccAddress;
+	
+	public Email(String dateTime, String subject, String body, String fromAddress, String toAddress, String ccAddress) {
+		super();
+		this.dateTime = dateTime;
+		this.subject = subject;
+		this.body = body;
+		this.fromAddress = fromAddress;
+		this.toAddress = toAddress;
+		this.ccAddress = ccAddress;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	
+	public String getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(String dateTime) {
+		this.dateTime = dateTime;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public String getFromAddress() {
+		return fromAddress;
+	}
+
+	public void setFromAddress(String fromAddress) {
+		this.fromAddress = fromAddress;
+	}
+
+	public String getToAddress() {
+		return toAddress;
+	}
+
+	public void setToAddress(String toAddress) {
+		this.toAddress = toAddress;
+	}
+
+	public String getCcAddress() {
+		return ccAddress;
+	}
+
+	public void setCcAddress(String ccAddress) {
+		this.ccAddress = ccAddress;
+	}
+
+}
+
