ce188335cf7900fa11238085275f49286688cf28
Kevin Richmond <40185674+krichmond53@users.noreply.github.com>
2019-03-23 17:35:28 -0400

    Initial Commit

---
+++ src/CSVtoTXTdriver.java
@@ -0,0 +1,47 @@
+/*
+ * Exporting emails from Outlook can be done either into a .pst or .csv.  .pst files
+ * can only be opened from within Outlook, which does no good if the person you wish
+ * to send the email to does not use outlook.  .csv files are not as human-readable and
+ * the export does not contain a date column.
+ * 
+ * This program is to take transform an Outlook .csv into a .txt file that includes a
+ * relative date based on the first date which appears in an email.  This will also be
+ * a more human readable format.
+ */
+public class CSVtoTXTdriver {
+
+	public static void main(String[] args) {
+		// Get Outlook .csv file
+		
+		// Parse .csv file for relevant data
+		Subject	
+		Body	
+		
+		// In Body, search for a string that looks like a date. 
+		// Format: Sent: Tuesday, March 19, 2019 2:39:38 PM
+
+		//change1
+		From: (Name)	
+		From: (Address)	
+		From: (Type)	
+		To: (Name)	
+		To: (Address)	
+		To: (Type)	
+		CC: (Name)	
+		CC: (Address)	
+		CC: (Type)	
+		BCC: (Name)	
+		BCC: (Address)	
+		BCC: (Type)	
+		Billing Information	
+		Categories	
+		Importance	
+		Mileage	
+		Sensitivity
+
+		
+
+
+	}
+
+}
